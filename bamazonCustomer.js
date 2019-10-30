a065e49eb80eee798bac249e76d052c4282b145e
Liz Harris <lizharris90210@gmail.com>
2019-10-30 13:11:17 -0400

    prompts working, shows table

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -3,14 +3,8 @@ var inquirer = require("inquirer");
 
 var connection = mysql.createConnection({
   host: "localhost",
-
-  // Your port; if not 3306
   port: 3306,
-
-  // Your username
   user: "root",
-
-  // Your password
   password: "password",
   database: "bamazon_db"
 });
@@ -18,55 +12,76 @@ var connection = mysql.createConnection({
 // Error
 connection.connect(function(err) {
   if (err) throw err;
-  console.log("connected as id " + connection.threadId);
-  connection.end();
+  start();
 });
-
-start();
 
 // Prompt for ID of product
 function start() {
+  connection.query("SELECT * FROM products", function(err, res) {
+    if (err) throw err;
+
+  printProducts(res);
+
   inquirer
-  .prompt([
-    {
-      name: "item_id",
-      type: "input",
-      message: "What is the Item ID number of the item you would like to purchase?"
-    },
-    {
-      // Prompt for unit quantity
-      name: "quantity",
-      type: "input",
-      message: "How many would you like?"
-    }
-  ]);
-  then(function(answer) {
-// Once order placed, check to see if enough product in stock to fill order- 
-  queryItemID();
-      if (answer.item_id > stock_quantity) {
-          // if not console log "insufficient quantity!" then prevent order-
-        console.log("Insufficient inventory for quantity specified");
-        } else {
-// if products available, fulfill order by updating sql DB for remaining quantity-
-      if (answer.item_id > stock_quantity) {
-          // Update SQL quantites
-        };
-        };
-      });
-    
-      
-
-// customer input item_id search
-function queryItemID() {
-    connection.query("SELECT * FROM item_id", function(err, res) {
-      if (err) throw err;
-      for (var i = 0; i < res.length; i++) {
-        console.log(res[i].id + " | " + res[i].product_name + " | " + res[i].department_name + " | " + res[i].price);
+    .prompt([
+      {
+        name: "whichItem",
+        type: "input",
+        message: "What item would you like to purchase?"
+      },
+      {
+        name: "itemAmount",
+        type: "input",
+        message: "How many items would you like to purchase?"
       }
+    ])
+    .then(function(answer) {
+      // search DB for item ID. if valid go to next question, if invalid, return "invalid item ID"
+      connection.query("SELECT * FROM products WHERE ?", 
+            [
+              {
+                item_id: answer.whichItem
+              }
+            ],
+      function(err, product) {
+        console.log(product)
+        });
+      // Once order placed, check to see if enough product in stock to fill order-
+      "UPDATE products WHERE `item_id` = answer.whichItem"
+      connection.query(
+         if (stock_quantity - itemAmount <= stock_quantity) {
+          } else {
+            (stock_quantity - itemAmount > stock_quantity)
+          }
+        );
+      }
+    });      //   // if not console log "insufficient quantity!" then prevent order-
+      //   console.log("Insufficient inventory for quantity specified");
+      // } else {
+      //   // if products available, fulfill order by updating sql DB for remaining quantity-
+      //   if (answer.item_id > stock_quantity) {
+      //     // Update SQL quantites
+      //   }
+      // }
+      // end connection
+      connection.end();
+    
     });
-  }
- 
+  })
 
-// show customer the cost of purchase
-
+    function printProducts(res) {
+        for (var i = 0; i < res.length; i++) {
+          console.log(
+            res[i].item_id +
+              " | " +
+              res[i].product_name +
+              " | " +
+              res[i].department_name +
+              " | " +
+              res[i].price
+          );
+        }
+    }
+  
+  // show customer the cost of purchase
 }
